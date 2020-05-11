5a4dbc417763d4e3097834f279d9025460f0e599
Noval <ainun@gmail.com>
2020-05-11 11:31:11 +0700

    Change Title Login WFH

--- application/views/login_wfh.php
+++ application/views/login_wfh.php
@@ -7,7 +7,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard Login</title>
+    <title>Dashboard Login WFH</title>
     <link rel="shortcut icon" href="<?php echo site_url('assets/images/fav.png') ?>" type="image/x-icon">
     <link rel="stylesheet" href="<?php echo site_url('assets/fontawesome/css/all.css') ?>">
 
