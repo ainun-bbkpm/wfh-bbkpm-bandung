f44548bd3536756d3a845ddd6fba47e8e266b422
Ainun BBKPM Bandung <65143326+ainun-bbkpm@users.noreply.github.com>
2020-07-24 08:15:53 +0700

    Add Max upload file

--- application/controllers/Wfh.php
+++ application/controllers/Wfh.php
@@ -663,7 +663,8 @@ class Wfh extends CI_Controller
 
                 $config['upload_path']          = "./uploads/wfh/$name_folder";
                 $config['allowed_types']        = 'xls|xlsx|pdf|jpg|jpeg|png|docx|doc|rar|zip|ppt|pptx';
-                $config['max_size']             = 5000;
+                // $config['max_size']             = 5000; // 5MB
+                $config['max_size']             = 80000; //80MN
                 $this->load->library('upload', $config);
 
                 // Load Library
